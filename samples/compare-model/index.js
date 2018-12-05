a59b21cf8b9ff08accdd13e390b33710a321d530
nxddsnc <fanqileiOGL@163.com>
2018-12-05 17:44:26 +0800

    Add ui notify

--- samples/compare-model/index.js
+++ samples/compare-model/index.js
@@ -9,27 +9,32 @@ window.onload = function () {
         function () {
             var btn1 = document.getElementById("compare-model1");
             var btn2 = document.getElementById("compare-model2");
-            
+            var messageDiv = document.getElementById("message");
+
             var modelId1 = "93rjkK14";
             var modelId2 = "DQ8P6P1k";
             var modelId3 = "Wr99BAr7"
             btn1.onclick = function () {
                 Modelo.Model.compare(modelId1, modelId2, function(data) {
+                    messageDiv.innerHTML = JSON.stringify(data);
                     console.log(data);
                 }, function(err) {
                     console.log(err);
                 });
+                messageDiv.innerHTML = "comparing...";
             }
             btn2.onclick = function () {
                 Modelo.Model.compare(modelId1, modelId3, function(data) {
+                    messageDiv.innerHTML = JSON.stringify(data);
                     console.log(data);
                 }, function(err) {
                     console.log(err);
                 });
+                messageDiv.innerHTML = "comparing...";
             }
         },
         function (errMsg) {
             console.log('signInErr: ' + errMsg);
-        },
+        }
     );
 }
